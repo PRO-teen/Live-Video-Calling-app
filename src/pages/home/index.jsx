40b3777e0c807694f0cd235596557109795bb970
PRO-teen <ayushyadav5867@gmail.com>
2024-07-30 19:39:09 +0530

    Updated body color to zinc and improved form styling

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,18 +10,20 @@ const HomePage = () => {
         navigate(`/room/${roomCode}`);
     }
   return (
-    <div className="home page">
+    <div className="homepage">
       <form onSubmit={handleFormSubmit} className="form">
-      <div>
-        <label>Enter Room Code</label>
+      <div >
+        <h2 className="text-center text-xl font-semibold mb-4">Enter Room Code </h2>
         <input 
         value={roomCode}
         onChange={(e)=>setRoomCode(e.target.value)}
         type="text" 
          required
-          placeholder="enter room code" />
+          placeholder="enter room code" 
+            className="border rounded px-4 py-2 text-center"
+          />
       </div>
-      <button type="submit">Enter room</button>
+      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Enter room</button>
       </form>
     </div>
   );
